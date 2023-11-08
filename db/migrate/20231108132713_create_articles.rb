30140271dcec15dda4d1c1792d06d3bb2662bf56
Bhaskar <bhaskarshines11@gmail.com>
2023-11-08 19:01:19 +0530

    sacffold articles

---
+++ db/migrate/20231108132713_create_articles.rb
@@ -0,0 +1,13 @@
+class CreateArticles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :articles do |t|
+      t.integer :category_id
+      t.text :title
+      t.text :description
+      t.string :author_id
+      t.string :integer
+
+      t.timestamps
+    end
+  end
+end
